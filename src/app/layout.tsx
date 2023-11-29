6b7a9139363375841a9a08458b3b98520f413898
Brad Samuelson <brad@lineheight.co>
2023-11-29 09:43:08 -0500

    update for meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
 import type { Metadata } from 'next'
-import Head from 'next/head'
-// import { Inter } from 'next/font/google'
-// import { Rubik } from 'next/font/google'
 // import './globals.css'
 import './styles.scss';
 
@@ -11,7 +8,32 @@ import './styles.scss';
 
 export const metadata: Metadata = {
 	title: 'Sketch Foundry | Custom Illustration',
-	description: 'Sketch Foundry offers subscription based custom illustrations.',
+	description: 'Sketch Foundry offers subscription based custom illustrations',
+	openGraph: {
+		title: 'Sketch Foundry | Custom Illustration',
+		description: 'Sketch Foundry offers subscription based custom illustrations',
+		url: 'https://www.sketchfoundry.co/',
+		images: [
+		  {
+			url: 'https://www.sketchfoundry.co/sf-meta-img.jpg',
+			width: 1200,
+			height: 628,
+		  },
+		],
+		locale: 'en_US',
+		type: 'website',
+	  },
+	  twitter: {
+		card: 'summary_large_image',
+		title: 'Sketch Foundry | Custom Illustration',
+		description: 'Sketch Foundry offers subscription based custom illustrations',
+		creator: '@cartoonbradley',
+		images: ['https://www.sketchfoundry.co/sf-meta-img.jpg'],
+	  },
+	  icons: {
+		icon: 'https://www.sketchfoundry.co/logo512.png',
+		apple: 'https://www.sketchfoundry.co/logo192.png',
+	  },
 }
 
 export default function RootLayout({
@@ -21,27 +43,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-		<Head>
-			{/* Primary Meta Tags */}
-			<title>Sketch Foundry | Custom Illustration</title>
-			<meta name="title" content="Sketch Foundry | Custom Illustration" />
-			<meta name="description" content="Sketch Foundry offers subscription based custom illustrations." />
-
-			{/* <!-- Open Graph / Facebook --> */}
-			<meta property="og:type" content="website" />
-			<meta property="og:url" content="https://www.sketchfoundry.co/" />
-			<meta property="og:title" content="Sketch Foundry | Custom Illustration" />
-			<meta property="og:description" content="Sketch Foundry offers subscription based custom illustrations." />
-			<meta property="og:image" content="/sf-meta-img" />
-
-			{/* <!-- Twitter --> */}
-			<meta property="twitter:card" content="summary_large_image" />
-			<meta property="twitter:url" content="https://www.sketchfoundry.co/" />
-			<meta property="twitter:title" content="Sketch Foundry | Custom Illustration" />
-			<meta property="twitter:description" content="Sketch Foundry offers subscription based custom illustrations." />
-			<meta property="twitter:image" content="/sf-meta-img" />
-
-		</Head>
 		<body>{children}</body>
 	</html>
   )
